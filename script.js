f395320fbda134bbbb85b4b6427f2392cdddbf67
Milind Gupta <62803826+MilindGupta-Creator@users.noreply.github.com>
2020-07-11 12:21:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var myInput = document.getElementById("psw");
+
+var length = document.getElementById("length");
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+
+  
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
